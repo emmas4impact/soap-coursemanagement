9a2eca81857687158da1666744eeec1802265419
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-04-14 01:07:19 +0300

    Update CourseDetailsEndpoints.java

--- soap-course-management/src/main/java/com/empact/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoints.java
+++ soap-course-management/src/main/java/com/empact/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoints.java
@@ -1,5 +1,7 @@
 package com.empact.soap.webservices.soapcoursemanagement.soap;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -8,8 +10,13 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.empact.soap.webservices.soapcoursemanagement.soap.bean.Course;
 import com.empact.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
+import com.empact.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService.Status;
 
 import https.courses_in28minutes_com.courses.CourseDetails;
+import https.courses_in28minutes_com.courses.DeleteCourseDetailsRequest;
+import https.courses_in28minutes_com.courses.DeleteCourseDetailsResponse;
+import https.courses_in28minutes_com.courses.GetAllCourseDetailsRequest;
+import https.courses_in28minutes_com.courses.GetAllCourseDetailsResponse;
 import https.courses_in28minutes_com.courses.GetCourseDetailsRequest;
 import https.courses_in28minutes_com.courses.GetCourseDetailsResponse;
 
@@ -26,20 +33,56 @@ public class CourseDetailsEndpoints {
 	public GetCourseDetailsResponse 
 	processCourseDeatilsRequest(@RequestPayload GetCourseDetailsRequest request) {
 		Course course  = service.findById(request.getId());
-		return mapCourse(course);
+		return mapCourseDetails(course);
 	}
-	private GetCourseDetailsResponse mapCourse(Course course) {
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
 		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
 		
+		response.setCourseDetails(mapCourse(course) );
+		return response;
+	}
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		for(Course course:courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
 		
-		
+
+		return response;
+	}
+	private CourseDetails mapCourse(Course course) {
 		CourseDetails courseDetails = new CourseDetails();
 		courseDetails.setId(course .getId());
 		courseDetails.setName(course.getName());
 		courseDetails.setDescription(course.getDescription());
+		return courseDetails;
+	}
+	
+	@PayloadRoot(namespace="https://courses.in28minutes.com/courses", localPart="GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse 
+	processAllCourseDeatilsRequest(@RequestPayload GetAllCourseDetailsRequest request) {
+		List<Course> courses  = service.findAll();
 		
-		response.setCourseDetails(courseDetails );
+		return mapAllCourseDetails(courses);
+	}
+	
+	@PayloadRoot(namespace="https://courses.in28minutes.com/courses", localPart="DeleteCourseDetailsRequest")
+	@ResponsePayload
+	public DeleteCourseDetailsResponse 
+	deleteCourseDeatilsRequest(@RequestPayload DeleteCourseDetailsRequest request) {
+		Status status = service.deleteById(request.getId());
+		
+		DeleteCourseDetailsResponse  response = new DeleteCourseDetailsResponse(); 
+		response.setStatus(mapStatus(status));
 		return response;
+	}
+	private https.courses_in28minutes_com.courses.Status mapStatus(Status status) {
+		// TODO Auto-generated method stub
+		if(status== Status.NOT_FOUND)
+			return https.courses_in28minutes_com.courses.Status.NOT_FOUND;
+		return https.courses_in28minutes_com.courses.Status.SUCCESS;
 	}
 
 }
